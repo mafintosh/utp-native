08d25941213b4b9c03afc48090f7a700b614afcf
Mathias Buus <mathiasbuus@gmail.com>
2015-04-15 12:35:03 -0700

    add naive streaming interface

--- example.js
+++ example.js
@@ -1,57 +1,14 @@
-var utpSocket = require('./')
-
-var socket = utpSocket()
-
-socket.handlers({
-  onconnect: function () {
-    console.log('[js] we are connected!')
-    var b = new Buffer('I am client!\n')
-    socket.write(b, b.length)
-    process.stdin.on('data', function (data) {
-      socket.write(data, data.length)
-    })
-  },
-  onread: function (data, length) {
-    process.stdout.write('[js] remote: ' + data.toString('utf-8', 0, length))
-  },
-  oneof: function () {
-    console.log('[js] eof received')
-  },
-  ondestroying: function () {
-    console.log('[js] destroying')
-  },
-  onsocket: function (sock) {
-    console.log('[js] new socket!')
-
-    sock.handlers({
-      onconnect: function () {
-        console.log('[js] we are connected!')
-      },
-      onread: function (data, length) {
-        process.stdout.write('[js] remote: ' + data.toString('utf-8', 0, length))
-      },
-      oneof: function () {
-        console.log('[js] eof received')
-      },
-      ondestroying: function () {
-        console.log('[js] destroying')
-      },
-      onsocket: function () {
-        console.log('[js] SHOULD NOT HAPPEN')
-      }
-    })
-
-    var b = new Buffer('I am server!\n')
-    sock.write(b, b.length)
-
-    process.stdin.on('data', function (data) {
-      sock.write(data, data.length)
-    })
-  }
-})
+var utp = require('./')
 
 if (process.argv[2] === 'listen') {
-  socket.listen(process.argv[3] || '10000')
+  var server = utp.createServer()
+
+  server.on('connection', function (socket) {
+    process.stdout.pipe(socket).pipe(process.stdout)
+  })
+
+  server.listen(process.argv[3] || 10000)
 } else {
-  socket.connect(process.argv[2] || '10000', process.argv[3] || '127.0.0.1')
+  var socket = utp.connect(process.argv[3] || 10000, process.argv[4])
+  process.stdin.pipe(socket).pipe(process.stdout)
 }
--- index.js
+++ index.js
@@ -1,4 +1,71 @@
+var stream = require('stream')
+var events = require('events')
+var util = require('util')
 var bindings = require('bindings')
 var utp = bindings('utp')
 
-module.exports = utp.socket
+var Connection = function (handle, port, host) {
+  stream.Duplex.call(this)
+
+  var self = this
+  this._handle = handle
+  this._connected = false
+
+  this._handle.handlers({
+    onconnect: function () {
+      self._connected = true
+      self.emit('connect')
+    },
+    onread: function (data, length) {
+      self.push(data.slice(0, length))
+    }
+  })
+
+  if (port) {
+    this._handle.connect(port, host)
+  } else {
+    this._connected = true
+  }
+}
+
+util.inherits(Connection, stream.Duplex)
+
+Connection.prototype._read = function () {
+  // TODO: backpressure :)
+}
+
+Connection.prototype._write = function (data, enc, cb) {
+  // TODO: backpressure :)
+  if (!this._connected) return this.once('connect', this._write.bind(this, data, enc, cb))
+  this._handle.write(data, data.length)
+  cb()
+}
+
+exports.connect = function (port, host) {
+  if (!port) throw new Error('Port is required')
+  return new Connection(utp.socket(), '' + port, host || '127.0.0.1')
+}
+
+var Server = function () {
+  events.EventEmitter.call(this)
+  var self = this
+  this._handle = utp.socket()
+  this._handle.handlers({
+    onsocket: function (handle) {
+      var conn = new Connection(handle)
+      self.emit('connection', conn)
+    }
+  })
+}
+
+util.inherits(Server, events.EventEmitter)
+
+Server.prototype.listen = function (port) {
+  this._handle.listen('' + (port || 0))
+}
+
+exports.createServer = function (onconnection) {
+  var server = new Server()
+  if (onconnection) server.on('connection', onconnection)
+  return server
+}
