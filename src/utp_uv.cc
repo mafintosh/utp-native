cd074709f57ccb614b4ccec3d3b506c0bb9623c9
David Whitlark <whitlark@gmail.com>
2017-10-18 04:16:53 -0400

    fixing build for windows

--- src/utp_uv.cc
+++ src/utp_uv.cc
@@ -189,11 +189,7 @@ on_utp_sendto (utp_callback_arguments *a) {
   struct sockaddr_in *addr = (struct sockaddr_in *) a->address;
   uv_udp_send(req, &(self->handle), &buf, 1, *addr, on_uv_send_compat);
 #else
-  uv_buf_t buf = {
-    .base = (char *) a->buf,
-    .len = a->len
-  };
-
+  uv_buf_t buf = uv_buf_init((char *) a->buf, a->len);
   uv_udp_try_send(&(self->handle), &buf, 1, a->address);
 #endif
   return 0;
@@ -391,10 +387,7 @@ utp_uv_send (utp_uv_t *self, uv_udp_send_t* req, char *data, size_t len, int por
   if (ret) return -1;
 #endif
 
-  uv_buf_t buf = {
-    .base = data,
-    .len = len
-  };
+  uv_buf_t buf = uv_buf_init(data, len);
 
 #ifdef UV_LEGACY
   return uv_udp_send(req, handle, &buf, 1, addr, on_uv_send);
